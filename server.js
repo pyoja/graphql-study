4aa19cd5ecdbb89858caddb856d4f0efbd91ff3f
pyoja <wjdgns13nr@naver.com>
2024-01-16 19:39:08 +0900

    Scalar and Root Type

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ import { ApolloServer, gql } from "apollo-server";
 // GET /allFilms
 
 const typeDefs = gql `
-  type Query{
-    allFilms: String
+  type User {
+    id: ID
+    username: String
+  }
+  type Tweet {
+    id: ID
+    text: String
+    author: User
+  }
+  type Query {
+    allTweets: [Tweet]
+    tweet(id: ID): Tweet
   }
 `;
 const server = new ApolloServer({typeDefs});
